f41294ec0d5ed36a64e837f19460c9e37948a130
ralphbetta <ralphbetta@gmail.com>
2023-01-22 15:11:59 +0100

    summarized app bar implementation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ function App() {
   return (
     <div >
     <Navbar/>
+    <div className="bg-[#f2f2f2] p-6">   
+      <div className="p-4 bg-white max-w-sm rounded-md shadow-md flex">
+      <div>
+        <img src="./icon.png" alt="just icon"></img>
+      </div>
+      <div></div>
+      </div>
+      </div>
+
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,10 +13,30 @@ const Navbar = () => {
     };
 
   return (
-    <div className='text-white flex-'>
-        <h1>React.</h1>
-        <div onClick={handleNav}>
-            {!nav ? <AiOutlineClose size={20}/> :  <AiOutlineMenu size={20}/>}
+    <div className='h-24 max-w=[1240px] text-white mx-auto px-4 flex justify-between items-center'>
+        <h1 className='font-bold'>React.</h1>
+        {/* mobile first: hidden else flex */}
+        <ul className='hidden md:flex'>
+            <li className='p-4'>Home</li>
+            <li className='p-4'>Company</li>
+            <li className='p-4'>Resources</li>
+            <li className='p-4'>About</li>
+            <li className='p-4'>Contact</li>
+        </ul>
+        {/* mobile first: block else hidden */}
+        <div className=' block md:hidden ease-in-out duration-500' onClick={handleNav}>
+            {nav ? <AiOutlineClose size={20}/> :  <AiOutlineMenu size={20}/>}
+        </div>
+
+        <div className= {nav ? 'fixed md:hidden left-0 top-0 w-[60%] h-full bg-[#000300] ease-in-out duration-500 ': 'fixed md:hidden left-[-100%]'} >
+        <h1 className='mt-9 mx-4 font-bold text-[#00df9a]'>React.</h1>
+            <ul className=''>
+                <li className='p-4 border-b border-gray-600'>Home</li>
+                <li className='p-4 border-b border-gray-600'>Company</li>
+                <li className='p-4 border-b border-gray-600'>Resources</li>
+                <li className='p-4 border-b border-gray-600'>About</li>
+                <li className='p-4 border-b border-gray-600'>Contact</li>
+            </ul>
         </div>
     </div>
   )
