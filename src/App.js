a1abd6ea957b850f830558570857f36b7ce451b9
ralphbetta <ralphbetta@gmail.com>
2023-01-22 18:15:14 +0100

    summarized hero section of landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Hero } from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div >
     <Navbar/>
+    <Hero/>
     <div className="bg-[#f2f2f2] p-6">   
       <div className="p-4 bg-white max-w-sm rounded-md shadow-md flex">
       <div>
